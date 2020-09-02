a75a709c7d7f6bd8b501194ea463efcd29e9c3d1
[DariaNico] <[tdaria207@gmail.com]>
2020-09-02 03:46:46 -0500

    Add comparers for Card attrs

--- GameLogic.py
+++ GameLogic.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import Card
 import Deck
 
@@ -5,6 +6,36 @@ Card = Card.Card
 Deck = Deck.Deck
 
 class GameLogic:
-    
-    def isASet(card1, card2, card3):
-        return False
+    def __init__(self):
+        self.playDeck = Deck()
+        self.discardPile = []
+        # TODO: Get this hooked up with the GUI
+        self.playGrid = []
+
+        # TODO: Need to have GUI to finish this logic
+        self.accruedSets = []
+        self.selected = []
+
+    @staticmethod
+    def isMatch(selectedAttrs):
+        reduce((lambda x, y: x == y), selectedAttrs)
+
+    @staticmethod
+    def isSet(selectedAttrs):
+        len(set(selectedAttrs)) == 3
+
+    def evaluateSelectedColors(self):
+        selectedColors = list(map(lambda card: card.getColor(), self.selected))
+        return { 'match': self.isMatch(selectedColors), 'set': self.isSet(selectedColors) }
+
+    def evaluateSelectedFills(self):
+        selectedFills = list(map(lambda card: card.getFill(), self.selected))
+        return { 'match': self.isMatch(selectedFills), 'set': self.isSet(selectedFills) }
+
+    def evaluateSelectedNumbers(self):
+        selectedNumbers = list(map(lambda card: card.getNumber(), self.selected))
+        return { 'match': self.isMatch(selectedNumbers), 'set': self.isSet(selectedNumbers) }
+
+    def evaluateSelectedShapes(self):
+        selectedShapes = list(map(lambda card: card.getShape(), self.selected))
+        return { 'match': self.isMatch(selectedShapes), 'set': self.isSet(selectedShapes) }
