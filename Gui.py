6b571a41c6b62c83d039278e8d39777b29d0afa5
William Goldberg <will.goldberg@nisc.coop>
2020-09-08 19:34:37 -0500

    Fix errors with ToolTip where it labels don't always disappear correctly.

--- Gui.py
+++ Gui.py
@@ -28,9 +28,7 @@ class Gui:
         self.tooltip = ToolTip()
         self.game_logic = GameLogic()
 
-    # Adds cards to play board.
-    # Parameter: List of 12 or 15 cards.
-    def place_cards_on_board(self, cards):
+    def set_cards_and_columns(self, cards):
         self.cards = cards
         if len(cards) == 12:
             self.num_cards = 12
@@ -66,38 +64,35 @@ class Gui:
                 self.next_board()
         
     def create_buttons(self):
-        # Adding the buttons in a loop in the way below results in all the lambda events to recieve the last index
-        # for i in range(0,num_cards):
-        #     buttonId = i
-        #     button = tk.Button(window, text = f"test button {i}", command = lambda: thanks(i))
-        #     buttons.append(button)
-
-        # So I need to do it manually for now
         window = self.game_frame
         cards = self.cards
         self.buttons = []
         for i in range(0,self.num_cards):
             image = PhotoImage(file = f"CardImages/{cards[i].color} {cards[i].fill} {cards[i].shape}{cards[i].number}.gif")
-            self.buttons.append(Button(window, image = image, command = lambda i=i: self.selected(i), highlightbackground='#3E4149'))
+            self.buttons.append(Button(window, image = image, command = lambda i=i: self.selected(i)))
             self.buttons[i].image = image
             self.buttons[i].bind('<Enter>', lambda event, i=i : self.show_tooltip(i))
-            self.buttons[i].bind('<Leave>', self.hide_tooltip)
+            self.buttons[i].bind('<Leave>', lambda event, i=i : self.hide_tooltip(i))
             
     def show_tooltip(self, id):
         card = self.cards[id]
         print("in show tooltip")
-        if self.ttb.is_on:
+        if self.ttb.is_on and not self.cards[id] in self.game_logic.selected:
             self.tooltip.show_tooltip(f"{card.color}\n{card.fill}\n{card.shape}\n{card.number}", self.root.winfo_pointerx(), self.root.winfo_pointery())
             
-    def hide_tooltip(self, event):
+    def hide_tooltip(self, id):
         print("in hide_tooltip")
+        if self.cards[id] in self.game_logic.selected:
+            return
         if self.ttb.is_on:
-            self.tooltip.hide_tooltip()
+            index = len(self.tooltip.windows) -1
+            self.tooltip.hide_tooltip(index)
             
     def next_board(self):
+        self.tooltip.wipe_all()
         self.wipe_board()
         self.scoreboard_frame.update_score(len(self.game_logic.successful_set_pile))
-        self.place_cards_on_board(self.game_logic.cards_in_play)
+        self.set_cards_and_columns(self.game_logic.cards_in_play)
         self.create_grid(self.buttons, self.cards)
         
     def new_game(self):
@@ -110,10 +105,9 @@ class Gui:
             button.grid_forget()
         
     def start_game(self):
-        #Configure window and label
         self.game_logic.new_game()
         self.scoreboard_frame.update_score(len(self.game_logic.successful_set_pile))
-        self.place_cards_on_board(self.game_logic.cards_in_play)
+        self.set_cards_and_columns(self.game_logic.cards_in_play)
         self.create_grid(self.buttons, self.cards)
 
         self.root.mainloop()
