ed7df4be47c3ac1db7b0ade5f45dadc5c1233e71
William Goldberg <will.goldberg@nisc.coop>
2020-09-09 08:38:02 -0500

    Add near end game warning

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -10,4 +10,7 @@ class ScoreBoard:
         self.scoreboard.pack(side = TOP)
         
     def update_score(self, score):
-        self.scoreboard['text'] = score
+        if score == 23:
+            self.scoreboard['text'] = f"{score} - one set needed to win. Careful, if you fail this round you lose!"
+        else:
+            self.scoreboard['text'] = score
